fef56bc1f37aa6b539c9b5a9284cce01382ed17a
Gavin S. <stahlgavin@gmail.com>
2023-10-14 17:45:06 -0700

    switch statement with 8ball game

---
+++ switch.js
@@ -0,0 +1,39 @@
+let userName = 'Jane';
+let userQuestion = 'Will I get rich soon?';
+let eightBall = '';
+let randomNumber = Math.floor(Math.random() * 8)
+
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!')
+console.log(userQuestion)
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain'
+    break;
+  case 1:
+    eightBall = 'It is decidedly so'
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again'
+    break;
+  case 3:
+    eightBall = 'Cannot predict now'
+    break;
+  case 4:
+    eightBall = 'Do not count on it'
+    break;
+  case 5:
+    eightBall = 'My sources say no'
+    break;
+  case 6:
+    eightBall = 'Outlook not so good'
+    break;
+  case 7:
+    eightBall = 'Signs point to yes'
+    break;
+  default:
+    eightBall = 'Please try again!'
+    break
+}
+
+console.log(eightBall)
