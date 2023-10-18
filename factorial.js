110428fdcbade27475eb58902e0ae2dfafb0fefd
Gavin S. <stahlgavin@gmail.com>
2023-10-18 00:26:07 -0700

    is prime example

--- factorial.js
+++ factorial.js
@@ -1,15 +1,17 @@
 // Given an integer 'n, find the factorial of that integer
-// in math, the factorial of a non-negative integer 'n, denoted n!, is the product all positive integers less than or equal to 'n'.
+// in math, the factorial of a non-negative integer 'n, denoted n!, is the product of all positive integers less than or equal to 'n'.
 // Factorial of zero is 1. factorial(4) = 4 * 3 * 2 * 1 = 24
 
 const factorial = (n) => {
-    let product = 1
-    for (let i = n; i > 0; i--){
-        product *= i
-    }
-    return product
-}
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
+};
 
-console.log(factorial(4)) // 24
-console.log(factorial(5)) // 120
-console.log(factorial(0)) // 1
+console.log(factorial(4)); // 24
+console.log(factorial(5)); // 120
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+// BigO - O(n)
--- fibonacci.js
+++ fibonacci.js
@@ -2,15 +2,15 @@
 // Fibonacci in math is a sequence in which each number is the sum of the two preceeding nums that starts with 0 and 1.
 // fib(2)=[0,1] fib(3)=[0,1,1], fib(5)=[0,1,1,2,3]
 
-const fibonacci = (n) => { // O(n)
+const fibonacci = (n) => {
   const fibs = [0, 1];
   for (let i = 2; i < n; i++) {
     fibs[i] = fibs[i - 1] + fibs[i - 2];
   }
   return fibs;
-}; 
+};
 
 console.log(fibonacci(2)); // [0,1]
 console.log(fibonacci(3)); // [0,1,1]
 console.log(fibonacci(7)); // [0,1,1,2,3,5,8]
-
+// BigO - O(n)
